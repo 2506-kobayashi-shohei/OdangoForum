3dc092ba10f9279948e162951d2bec8eed800276
ogasawara_shoko <ogasawara.shoko@alhinc.jp>
2025-07-18 16:12:56 +0900

    ユーザー管理画面バックエンド実装

--- src/main/java/com/example/odango/forum/controller/UserManageController.java
+++ src/main/java/com/example/odango/forum/controller/UserManageController.java
@@ -3,9 +3,6 @@ package com.example.odango.forum.controller;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,20 +28,5 @@ public class UserManageController {
         mav.addObject("users", userData);
         return mav;
     }
-
-    /* ユーザー管理画面表示 */
-//    @GetMapping("/Forum/userManage")
-//    public ModelAndView userManage() {
-//        // 管理者権限フィルター
-//
-//        // ユーザー情報全件取得
-//        ModelAndView mav = new ModelAndView();
-//        List<UsersForm> userData = userService.findAllUser();
-//        // ユーザー情報をリクエストに設定し、フォワード処理
-//
-//        mav.setViewName("/userManage");
-//        mav.addObject("users", userData);
-//        return mav;
-//    }
 
 }
